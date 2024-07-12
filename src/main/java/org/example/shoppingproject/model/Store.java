3b7cd048164db602cdeee9f85fb9eb580a737a0c
javoh <javohiragma@gmail.com>
2024-07-12 11:43:04 +0500

    model qismi 3

--- src/main/java/org/example/shoppingproject/model/Store.java
+++ src/main/java/org/example/shoppingproject/model/Store.java
@@ -1,5 +1,14 @@
 package org.example.shoppingproject.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Store {
     private Integer id;
     private String name;
