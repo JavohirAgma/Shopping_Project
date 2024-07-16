4f3eaede72c614ce1185a6e8f2b3c345fa44e4e5
odina18032005 <102730928+odina18032005@users.noreply.github.com>
2024-07-16 15:04:22 +0500

    *

--- src/main/java/org/example/shoppingproject/config/DbConnection.java
+++ src/main/java/org/example/shoppingproject/config/DbConnection.java
@@ -16,7 +16,7 @@ public interface DbConnection {
     static Properties getProperties(){
         Properties properties = new Properties();
         properties.put("user","postgres");
-        properties.put("password","Agma2008");
+        properties.put("password","990099413o");
         return properties;
     }
 
